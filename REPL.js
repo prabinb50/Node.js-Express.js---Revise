38af131f15ffc405e64b19c9ebdc8b6602a6f225
prabinb50 <joshiprabin17@gmail.com>
2024-10-08 13:37:20 +0545

    update

---
+++ REPL.js
@@ -0,0 +1,9 @@
+console.log("Hello my name is Prabin Joshi");
+
+// REPL
+// 1. JS Expression
+// 2. Use variables
+// 3. multiline code / loops
+// 4. use (_) to get the last result
+// 5. we can use editor mode
+
---
+++ coreModules.js
@@ -0,0 +1,33 @@
+// CRUD OPERATION IN NODE JS
+
+// CREATE a new file
+// note: writeFileSync ley k garxa vaney file present xa ki nai vaneyra check garxa first maa ani 
+// if file present xaina vaney craete garxa file lai, if file present xa vaney data lai override gardinxa....
+const fs = require("fs");
+
+fs.writeFileSync ("read.text", "Hello my name is Prabin"); 
+
+// override the above data (data:- Hello my name is Prabin)
+fs.writeFileSync ("read.text", "Hello my name is Prabin Joshi."); 
+
+// suppose read.txt vanney file maa already data xa rey ani feri tesmaa aru data haru add garnu paryo vaney
+// chai....append means add extra data in both js and node.js
+fs.appendFileSync("read.text", " Recently, I am studying at the capital city of Nepal, Kathmandu.")
+
+
+
+
+// READ
+// Node.js includes an additional data type called buffer
+// (not avaliable in browser's Js)
+// Buffer is mainly used to store binary data
+// while reading from a file or receiving packets over the network
+const bufferData = fs.readFileSync("read.text");
+// console.log(bufferData);
+orginalData = bufferData.toString();
+console.log(orginalData);
+
+// UPDATE
+
+
+// DELETE
